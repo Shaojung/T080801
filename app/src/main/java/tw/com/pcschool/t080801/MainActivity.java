57f960d8b333b392a437428f99fdc598550cef6a
shaojung <shaojung@gmail.com>
2016-08-08 10:43:08 +0800

    DB Test.

--- app/src/main/java/tw/com/pcschool/t080801/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t080801/MainActivity.java
@@ -2,6 +2,9 @@ package tw.com.pcschool.t080801;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CopyDatabase();
+
+    }
+
+    private void CopyDatabase()
+    {
+        final int BUFFER_SIZE = 400000;
+        final String DB_NAME = "test1.db"; // 保存的資料庫檔案名
+        String PACKAGE_NAME = getPackageName();
+        File DB_PATH = getDatabasePath(DB_NAME);
+        Log.d("DB", DB_PATH.toString());
     }
 }
