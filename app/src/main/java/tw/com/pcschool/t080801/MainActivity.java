ff0c2b4b0d92c4070c36d3050bdf5221713f5278
shaojung <shaojung@gmail.com>
2016-08-08 11:21:45 +0800

    Copy DB Finish.

--- app/src/main/java/tw/com/pcschool/t080801/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t080801/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.util.Log;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +21,38 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void CopyDatabase()
-    {
+    private void CopyDatabase() {
         final int BUFFER_SIZE = 400000;
-        final String DB_NAME = "test1.db"; // 保存的資料庫檔案名
+        final String DB_NAME = "test1.sqlite"; // 保存的資料庫檔案名
         String PACKAGE_NAME = getPackageName();
         File DB_PATH = getDatabasePath(DB_NAME);
-        Log.d("DB", DB_PATH.toString());
+
+        DB_PATH.mkdir();
+
+        Log.d("DB", "DB:" + DB_PATH.toString());
+
+
+        try {
+            if (!(DB_PATH.exists())) {
+                File DIR = new File(getApplicationInfo().dataDir + "/databases");
+
+                DIR.mkdir();
+                Log.d("DB", "DIR:" + DIR.toString());
+                // 判斷資料庫檔案是否存在，若不存在則執行導入，否則直接打開資料庫
+                InputStream is = getResources().openRawResource(R.raw.test1);
+                FileOutputStream fos = new FileOutputStream(DB_PATH);
+                byte[] buffer = new byte[BUFFER_SIZE];
+                int count = 0;
+                while ((count = is.read(buffer)) > 0) {
+                    fos.write(buffer, 0, count);
+                }
+                fos.close();
+                is.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
